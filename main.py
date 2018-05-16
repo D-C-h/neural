609f38a8f39b524c230e9cad8c72abf24517ecc6
D-C-h <35214072+D-C-h@users.noreply.github.com>
2018-05-16 15:58:20 +0530

    Update main.py
    added link and neuron classes

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
 #new stuff incoming
+import numpy as np
+
+
+#using relu as activation function
+#maybe use sigmoid later
+
+def activate(x):
+  return max(0,x)
+
+#neuron class with value and layer(maybe layer can be removed)
+
+class Neuron:
+  def __init__(self, value,layer):
+    self.value = value
+    self.layer = layer
+  
+class Link:
+  def __init__(self,weight)
+ 
+    
