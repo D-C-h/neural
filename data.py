9a40244241e69e1c05dc688afb70836f0b226e9a
chagarlamudi dharun <dharun@localhost.localdomain>
2018-11-29 10:51:57 -0500

    changed the data format,

--- data.py
+++ data.py
@@ -1,22 +1,9 @@
-import matplotlib.pyplot as plt
-#for plotting
-
 import numpy as np
-#scintific computation library 
 
+#basically using this file for sending data to the network
 
+def data():
+    return data = np.array([
+            [[0,0,0],[0,0,1],[0,1,0],[0,1,1],[1,0,0],[1,0,1],[1,1,0],[1,1,1]],[0.1.1.1.0,0,0,1]])
 
-import sklearn.datasets
-
-#generate data using sklearn
-
-
-# Generate a dataset and plot it
-np.random.seed(0)
-
-X, y = sklearn.datasets.make_moons(200, noise=0.20)
-
-plt.scatter(X[:,0], X[:,1], s=40, c=y, cmap=plt.cm.Spectral)
-
-plt.show()
-
+#this data can be sent to the network
