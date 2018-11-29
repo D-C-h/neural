231c279b5ea2d80bee3fa5cc267c96348e9074d4
chagarlamudi dharun <dharun@localhost.localdomain>
2018-11-29 10:55:13 -0500

    fixed erro

--- data.py
+++ data.py
@@ -3,7 +3,7 @@ import numpy as np
 #basically using this file for sending data to the network
 
 def data():
-    return data = np.array([
+    return np.array([
             [[0,0,0],[0,0,1],[0,1,0],[0,1,1],[1,0,0],[1,0,1],[1,1,0],[1,1,1]],[0.1.1.1.0,0,0,1]])
 
 #this data can be sent to the network
