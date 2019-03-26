30c1e782f65aa667ae2d6252496471a526495657
chagd <dharunchagarlamudi@gmail.com>
2019-03-26 18:59:00 +0530

    some minor deatils were fixed

--- README.md
+++ README.md
@@ -3,6 +3,6 @@ me trying to code a neural network from scratch
 
 making this with numpy only Tensorflow in future
 
-incomplete
+incomplete *for now*
 
 ``\__(-_-)__/``
--- data.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-#basically using this file for sending data to the network
-
-def data():
-    return np.array([
-            [[0,0,0],[0,0,1],[0,1,0],[0,1,1],[1,0,0],[1,0,1],[1,1,0],[1,1,1]],[0,1,1,1,0,0,0,1]])
-
-#this data can be sent to the network
